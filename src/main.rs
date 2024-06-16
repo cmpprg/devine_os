7a2c23cca6781204ea0a3baf23c17ecda5b2eb57
Ryan Camp <campryan@comcast.net>
2024-06-16 10:43:35 -0600

    Modularize all code to present

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+mod modules;
+use modules::vga_buffer::color::Color;
+use modules::vga_buffer::color_code::ColorCode;
+
 static HELLO: &[u8] =
     b"Hello Ryan Something is bound to go wrong here I wonder what it will be huh!!!!! There could be over the line buffer somet thing else";
 
@@ -21,10 +25,14 @@ pub extern "C" fn _start() -> ! {
 
     let vga_buffer = 0xb8000 as *mut u8;
 
+    let foreground = Color::Blue;
+    let background = Color::White;
+    let color_code = ColorCode::new(foreground, background);
+
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0x05;
+            *vga_buffer.offset(i as isize * 2 + 1) = color_code.value();
         }
     }
     loop {}
---
+++ src/modules/vga_buffer/color_code.rs
@@ -0,0 +1,15 @@
+use crate::modules::vga_buffer::color::Color;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct ColorCode(u8);
+
+impl ColorCode {
+    pub fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+
+    pub fn value(self) -> u8 {
+        self.0
+    }
+}

cc9314bbcbb40add0085f3a084948915120a02fb
Ryan Camp <campryan@comcast.net>
2024-06-16 09:36:07 -0600

    Add config for bootloader and build src

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-#![no_std]
-#![no_main]
+// src/main.rs
+
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+static HELLO: &[u8] =
+    b"Hello Ryan Something is bound to go wrong here I wonder what it will be huh!!!!! There could be over the line buffer somet thing else";
 
 use core::panic::PanicInfo;
 
@@ -9,7 +14,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x05;
+        }
+    }
     loop {}
 }
